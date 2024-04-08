0d7889afcca7642d464b37c90c84804336fde346
lhays <lhays9606@gmail.com>
2024-04-08 00:38:33 -0400

    add some basic scaffolding for home page layout. Add a few tailwind styles to get a feel for layout. modify routes slightly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+import plugin from 'tailwindcss/plugin'
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+
+  ],
 }
 
