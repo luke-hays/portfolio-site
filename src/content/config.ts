01d74d1104cfd56622770c77b2836d4957353d51
lhays <lhays9606@gmail.com>
2024-04-10 11:20:35 -0400

    add blog post schema and content

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define your collection(s)
+const blogCollection = defineCollection({ 
+  type: 'content',
+  schema: z.object({ 
+    title: z.string(),
+    publishDate: z.string().transform(str => {
+      const date = new Date(str)
+      return date.toISOString()
+    })
+   })
+ });
+
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  'blog': blogCollection,
+};
