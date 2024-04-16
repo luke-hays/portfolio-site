f5b1beb67465b871efa97a56764f98374afc1889
lhays <lhays9606@gmail.com>
2024-04-16 09:19:17 -0400

    update layout, add plugin for scroll styling in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
-
 import plugin from 'tailwindcss/plugin'
+const { scrollbarGutter, scrollbarWidth, scrollbarColor } = require('tailwind-scrollbar-utilities');
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -30,7 +30,10 @@ export default {
           fontWeight: theme('fontWeight.semibold')
         },
       })
-    })
+    }),
+    scrollbarGutter(), // no options to configure
+    scrollbarWidth(), // no options to configure
+    scrollbarColor(), // no options to configure
   ],
 }
 
